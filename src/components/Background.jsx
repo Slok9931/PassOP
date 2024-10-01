b88be3c6f38f790ab1e49dd2ef2d4c6c53bf041a
Slok Tulsyan <148342312+Slok9931@users.noreply.github.com>
2024-10-01 21:13:26 +0530

    Create Background.jsx

---
+++ src/components/Background.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Background = () => {
+  return (
+    <div>
+      <div className="absolute inset-0 -z-10 min-h-screen w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#86efac_100%)]"></div>
+    </div>
+  );
+};
+
+export default Background;
